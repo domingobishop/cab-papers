79ca59d8029b75f074441d520773bb633c0df477
Christopher Bishop <domingobishop@gmail.com>
2018-01-10 16:36:55 +0000

    regex clean text

--- cab.py
+++ cab.py
@@ -1,6 +1,15 @@
+import re
 import functions
 
-with open('cab/CAB_23_1_0_0003.txt', 'r') as text:
-    data = text.read().replace('\n', ' ')
+with open('cab/CAB_23_1_0_0007.txt', 'r') as text:
+    data = text.read().replace('\n', '. ')
+    data = data.replace('Hon.', 'Hon')
+    data = data.replace('.]', '.] ')
+    data = re.sub(r'(\w\.)(\w{2,})', r'\1 \2', data)
+    data = re.sub(r'(\))(\w)', r'\1 \2', data)
+    data = re.sub(r'([a-z])([A-Z])', r'\1 \2', data)
+    data = re.sub(r'([A-Z])\.', r'\1', data)
+    data = re.sub(r'\.([a-zA-Z])', r'. \1', data)
+    data = re.sub(r',([a-zA-Z])', r', \1', data)
 
 print(functions.summarize(data, 3, False, 300))
